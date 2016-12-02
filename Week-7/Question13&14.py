26d11e2061b0628a9f2afc151e8773f95a788754
rithinch <rithinch@users.noreply.github.com>
2016-12-02 21:22:36 +0000

    Update Question13&14.py

--- Week-7/Question13&14.py
+++ Week-7/Question13&14.py
@@ -1,18 +1,40 @@
+"""
+    210CT - Programming, Algorithms and Data Structures.
+    Question15.py
+    Purpose:  Implement Dijkstra’s algorithm for a weighted graph data structure.
+             
+    Author : Rithin Chalumuri
+    Version: 1.0 
+    Date   : 02/12/16
+    
+"""
+
 class Vertex(object):
 
     def __init__(self, value):
+        """
+        Initialize the vertex object.
+        value is the value to be assigned this vertex.
+        connections is the list of all edges associated.
+        """
         
         self.value = value
         
         self.connections = []
 
     def connect(self,connection):
+        """ 
+        Function to add an edge between this vertex and another vertex.
+        Add connection to connections list of this vertex.
+        
+        Parameters:
+            connection (string); the value of vertex which is to be connected
+        """
 
-        if connection in self.connections:
-            
+        if connection in self.connections: # To avoid duplicate values
             pass
         
-        else:
+        else: # If its not a repeating value then add to connections
             
             self.connections.append(connection)
             
@@ -22,20 +44,49 @@ class Vertex(object):
 class Graph():
 
     def __init__(self,name):
+        """
+        Initialize the Graph object.
+        name is the name of the graph.
+        vertices is the list of all vertex objects in this graph.
+        """
         
         self.vertices = []
         
         self.name= name
 
     def addVertex(self,value):
+        """
+        Function to add a new vertex object with value into list of vertices in graph.
+        
+        Parameters:
+        value (any data type); the value with which a vertex object will be created
+        """
         
         self.vertices.append(Vertex(value))
 
     def getVertex(self, value):
+        """
+        Function to return vertex object with value from list of vertices in graph.
+        
+        Parameters:
+        value (any data type); the value to look for in graph vertices.
+        
+        Returns:
+        Vertex Object with a value passed as argument.
+        """
         
         return self.vertices[self.indexof(value)]
 
     def contains(self,value):
+        """ 
+        Function to check if a particular vertex is in the graph.
+        
+        Parameters:
+        value (any data type); the value to look for in the graph.
+        
+        Returns:
+        A boolean; True if a vertex with 'value' exists, else false.
+        """
         
         for vertex in self.vertices:
             
@@ -46,6 +97,17 @@ class Graph():
         return False
             
     def indexof(self, value):
+        """
+        Function to return the index of a particular value in list of 
+        vertex objects in the graph.
+        
+        Parameters:
+        value (any data type); the value for which you want the index.
+        
+        Returns:
+        index (int); the index of the value looking for in vertices list
+        None; if a vertex with the value doesnt exist in the graph.
+        """
         
         index = 0
         
@@ -58,91 +120,144 @@ class Graph():
             index += 1
 
     def addEdge(self,u,v):
+        """
+        Function to add an edge with a weight between two vertrices.
         
-        if not self.contains(u):
+        Parameters:
+        u (any data type): the value of first vertex to connect
+        v (any data type): the value of second vertex to connect
+        """
+        
+        if not self.contains(u): #If the first vertex u doesnt exist in graph
             
             self.addVertex(u)
             
-        if not self.contains(v):
+        if not self.contains(v): #If the second vertex v doesnt exist in graph
             
             self.addVertex(v)
         
         uIndex = self.indexof(u)
         vIndex = self.indexof(v)
         
-        self.vertices[uIndex].connect(v)
-        self.vertices[vIndex].connect(u)
+        self.vertices[uIndex].connect(v) # Adding v to connections list of vertex u
+        self.vertices[vIndex].connect(u) # Adding u to connections list of vertex v
         
 
     def printGraph(self):
+        """
+        Function to print the vertices and its weights edges.
+        Each edge is a list with two values; [vertex value, weight]
+        Adjacency list of each vertex is is a list of edges associated with it.
+        """
+        
         print("Vertex" + "|".center(3) + "Adjacency List")
         print("-"*7 + "|" + "-"*18)
         for vertex in self.vertices:
+            
             print(str(vertex.value) + " "*5 + "|".center(3) + str(vertex.connections))
             
         return ""
 
     def printVertices(self):
+        """
+        Function to print a list of all vertices in graph
+        seperated by a comma.
+        """
 
         vertices = "Vertices in Graph '" + str(self.name) + "' : "
 
         for i in range(len(self.vertices)):
-            vertices += str(self.vertices[i].value)
-            if i != len(self.vertices)-1:
+            
+            vertices += str(self.vertices[i].value) #Add the value to vertices string
+            
+            if i != len(self.vertices)-1: # If it is last vertex the no need of comma
+                
                 vertices += ", "
                 
-        print( vertices)
-        return ""
+        print(vertices)
+        
 
     def dfs(self, v):
+        """
+        Function to perform depth first search traversal.
+        Results are saved into 'Traversal Results.txt'
+        
+        Parameters:
+        v (any data type); the starting vertex to perform depth first search traversal.
+        """
 
-        if not(self.contains(v)):
+        if not(self.contains(v)): #Checking if v exists in graph
             print("DFS Error: Vertex '"+str(v)+"' does not exist in '"+str(self.name)+"' graph. Please try again.")
             return
             
         stack = []
         visited = []
-        stack.append(self.getVertex(v))
+        stack.append(self.getVertex(v)) #Adding starting vertex to stack
 
         while not (len(stack)==0):
             
             currentVertex = stack.pop()
+            
             if currentVertex.value not in visited:
+                
                 visited.append(currentVertex.value)
 
-                for edge in currentVertex.connections[::-1]:
+                for edge in currentVertex.connections[::-1]: # Reversing connections lists to perform traversal in ascending order
+                    
                     stack.append(self.getVertex(edge))
                     
         self.saveTraversal(visited,"bfs")
+        
         print("DFS Traversal Results starting at vertex '"+str(v)+"' of graph '"+str(self.name)+"' are saved to 'Traversal Results.txt' file.")
     
 
     def bfs(self, v):
+        """
+        Function to perform breadth first search traversal.
+        Results are saved into 'Traversal Results.txt'
+        
+        Parameters:
+        v (any data type); the starting vertex to perform breadth first search traversal.
+        """
 
-        if not(self.contains(v)):
+        if not(self.contains(v)): #Checking if v exists in graph
             print("BFS Error: Vertex '"+str(v)+"' does not exist in '"+str(self.name)+"' graph. Please try again.")
             return
 
         queue = []
         visited = []
-        queue.append(self.getVertex(v))
+        queue.append(self.getVertex(v)) #Adding starting vertex to queue
         
         while not (len(queue)==0):
+            
             currentVertex = queue.pop()
+            
             if currentVertex.value not in visited:
+                
                 visited.append(currentVertex.value)
+                
                 for edge in currentVertex.connections:
+                    
                     queue.insert(0,self.getVertex(edge))
                     
-        self.saveTraversal(visited,"bfs")
+        self.saveTraversal(visited,"bfs") #Saving the results to 'Traversal Results.txt' file'
+        
         print("BFS Traversal Results starting at vertex '"+str(v)+"' of graph '"+str(self.name)+"' are saved to 'Traversal Results.txt' file.")
     
 
     def saveTraversal(self,arr,traversal):
-        try:
+        """
+        Function to write the traversal results into 'Traversal Results.txt' file
+        
+        Parameters:
+        arr (list); the list which contains results of the traversal
+        traversal (string); the type of traversal "bfs" or "dfs"
+        """
+        
+        try: #Checking if the file exists
             file = open("Traversal Results.txt","r")
             file.close()
-        except FileNotFoundError:
+        except FileNotFoundError: #If it doesnt create a new file
             file = open("Traversal Results.txt","w")
             file.close
 
@@ -154,9 +269,12 @@ class Graph():
         file = open("Traversal Results.txt","a")
 
         resultsString = ""
+        
         for i in range(len(arr)):
-            resultsString += str(arr[i])
-            if i != len(arr) - 1:
+            
+            resultsString += str(arr[i]) #Add the value to results string
+            
+            if i != len(arr) - 1: # If it is last vertex the no need of comma
                 resultsString += ", "
 
         file.write(title)
